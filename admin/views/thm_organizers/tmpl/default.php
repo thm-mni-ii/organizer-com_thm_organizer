74f00b03920cf298fd9444eb8ddaf69c52575dda
J.Antrim <J.Antrim@798ade6b-684f-418b-bd88-1ba854bd16d0>
2011-02-03 09:41:54 +0000

    revamped the main menu display * styles exported to the new backend stylesheet * new list form of the individual submenus * submenu entries now have a short explanation to their functionality * renamed submenu entries for better standardization and oversight
    git-svn-id: http://tracking.mni.fh-giessen.de/svn/joomla/projects/com_thm_organizer/trunk@3093 798ade6b-684f-418b-bd88-1ba854bd16d0

--- admin/views/thm_organizers/tmpl/default.php
+++ admin/views/thm_organizers/tmpl/default.php
@@ -10,40 +10,91 @@
  **/
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-
-global $option;
-$document =& JFactory::getDocument();
-$document->addStyleSheet("templates/giessenstyle/css/extension.css");
-?>
-
+global $option;?>
 <div id="thm_organizer_main" >
-    <div id="thm_organizer_main_logo">
-        <?php echo $this->logo; ?>
+    <div id="thm_organizer_main_description">
+        THM - Organizer is designed to handle the resource management and scheduling needs of the Univeristy of Applied Sciences at Giessen, Germany.
     </div>
-    <div id="thm_organizer_main_title">
-        Giessen Scheduler - Main Menu
-    </div>
-    <table id="thm_organizer_main_submenus" class="adminlist">
-        <tr>
-            <td>
-                <div id="cpanel">
-<?php
-
-$link = 'index.php?option='.$option.'&amp;view=category_list';
-thm_organizersViewthm_organizers::quickiconButton( $link, 'category.png', JText::_( 'Category Manager' ) );
-
-$link = 'index.php?option='.$option.'&amp;view=room_ip_list';
-thm_organizersViewthm_organizers::quickiconButton( $link, 'monitor-settings.png', JText::_( 'Monitor Manager' ) );
-
-$link = 'index.php?option='.$option.'&amp;view=semester_list';
-thm_organizersViewthm_organizers::quickiconButton( $link, 'semesters.png', JText::_( 'Semester Manager' ) );
-
-$link = 'index.php?option='.$option.'&amp;view=scheduler_application_settings';
-thm_organizersViewthm_organizers::quickiconButton( $link, 'scheduler-settings.jpg', JText::_( 'Scheduler Application Settings' ) );
-
-?>
+    <div id="cpanel">
+        <div class="thm_organizer_main_submenu" >
+            <div class="thm_organizer_main_linkdiv" >
+                <div class="icon">
+                    <a href="index.php?option=<?php echo $option; ?>&amp;view=category_manager"><?php
+                        echo JHTML::_('image',
+                                      'components/com_thm_organizer/assets/images/category_manager.png',
+                                      JText::_( 'Category Manager' ),
+                                      array( 'class' => 'thm_organizer_main_image'));
+                        ?><span><?php echo JText::_( 'Category Manager' ); ?></span></a>
                 </div>
-            </td>
-        </tr>
-    </table>
+            </div>
+            <div class="thm_organizer_main_descriptiondiv" >
+                The category manager associates event types with corresponding category types from the content<br />
+                component. This enables the administrator to set and use the access permissions of the associated<br />
+                content category.
+            </div>
+        </div>
+        <div class="thm_organizer_main_submenu" >
+            <div class="thm_organizer_main_linkdiv" >
+                <div class="icon">
+                    <a href="index.php?option=<?php echo $option; ?>&amp;view=monitor_manager"><?php
+                        echo JHTML::_('image',
+                                      'components/com_thm_organizer/assets/images/monitor_manager.png',
+                                      JText::_( 'Monitor Manager' ),
+                                      array( 'class' => 'thm_organizer_main_image'));
+                        ?><span><?php echo JText::_( 'Monitor Manager' ); ?></span></a>
+                </div>
+            </div>
+            <div class="thm_organizer_main_descriptiondiv" >
+                The monitor manager associates the IP addresses of monitors with the room where they are displayed<br />
+                and the content which is to be displayed on them.
+            </div>
+        </div>
+        <div class="thm_organizer_main_submenu" >
+            <div class="thm_organizer_main_linkdiv" >
+                <div class="icon">
+                    <a href="index.php?option=<?php echo $option; ?>&amp;view=semester_manager"><?php
+                        echo JHTML::_('image',
+                                      'components/com_thm_organizer/assets/images/semester_manager.png',
+                                      JText::_( 'Semester Manager' ),
+                                      array( 'class' => 'thm_organizer_main_image'));
+                        ?><span><?php echo JText::_( 'Semester Manager' ); ?></span></a>
+                </div>
+            </div>
+            <div class="thm_organizer_main_descriptiondiv" >
+                The semester manager creates semesters. Semesters, as used here, are a relation of an organizational<br />
+                unit such as a university department with an actual semester. In addition a user group can be selected which<br />
+                then has permission to administrate the schedule(s) associated with this semester.
+            </div>
+        </div>
+        <div class="thm_organizer_main_submenu" >
+            <div class="thm_organizer_main_linkdiv" >
+                <div class="icon">
+                    <a href="index.php?option=<?php echo $option; ?>&amp;view=scheduler_application_settings"><?php
+                        echo JHTML::_('image',
+                                      'components/com_thm_organizer/assets/images/scheduler_application_settings.jpg',
+                                      JText::_( 'Scheduler Application Settings' ),
+                                      array( 'class' => 'thm_organizer_main_image'));
+                        ?><span><?php echo JText::_( 'Scheduler Application Settings' ); ?></span></a>
+                </div>
+            </div>
+            <div class="thm_organizer_main_descriptiondiv" >
+                Dummy text.
+            </div>
+        </div>
+        <div class="thm_organizer_main_submenu" >
+            <div class="thm_organizer_main_linkdiv" >
+                <div class="icon">
+                    <a href="index.php?option=<?php echo $option; ?>&amp;view=virtual_schedule_manager"><?php
+                        echo JHTML::_('image',
+                                      'components/com_thm_organizer/assets/images/virtual_manager.png',
+                                      JText::_( 'Virtual Schedule Manager' ),
+                                      array( 'class' => 'thm_organizer_main_image'));
+                        ?><span><?php echo JText::_( 'Virtual Schedule Manager' ); ?></span></a>
+                </div>
+            </div>
+            <div class="thm_organizer_main_descriptiondiv" >
+                Dummy text.
+            </div>
+        </div>
+    </div>
 </div>
--- admin/views/thm_organizers/view.html.php
+++ admin/views/thm_organizers/view.html.php
@@ -18,52 +18,18 @@ class thm_organizersViewthm_organizers extends JView
         jimport('joomla.html.pane');
 
         //initialise variables
-        $document	= & JFactory::getDocument();
-        $pane   	= & JPane::getInstance('sliders');
-        $user 		= & JFactory::getUser();
+        $document = & JFactory::getDocument();
+        $document->addStyleSheet($this->baseurl."/components/com_thm_organizer/assets/css/thm_organizer.css");
+        $pane = & JPane::getInstance('sliders');
+        $user = & JFactory::getUser();
 
         //build toolbar
-        JToolBarHelper::title( JText::_( 'Giessen Scheduler - Main Menu' ), 'home.png' );
-
-        //Create Submenu
-	$model = $this->getModel();
-        foreach($model->data->links as $link)
-        {
-            JSubMenuHelper::addEntry( JText::_( $link['name'] ), 'index.php?'.$link['link']);
-        }
+        JToolBarHelper::title( JText::_( 'THM - Organizer: Main Menu' ), 'home.png' );
 
         //assign vars to the template
-        $this->assignRef('pane'			, $pane);
-        $this->assignRef('user'			, $user);
-        
-        $logo = JHTML::_('image', 'components/com_thm_organizer/assets/images/logo.png', 'logo' );
-        $this->assignRef('logo', $logo);
+        $this->assignRef('pane', $pane);
+        $this->assignRef('user', $user);
 
 	parent::display($tpl);
     }
-
-    /**
-    * Creates the buttons view
-    *
-    * @param string $link targeturl
-    * @param string $image path to image
-    * @param string $text image description
-    * @param boolean $modal 1 for loading in modal
-    */
-    function quickiconButton( $link, $image, $text )
-    {
-        //initialise variables
-        $lang = & JFactory::getLanguage();
-?>
-        <div style="float: left;">
-            <div class="icon">
-                <a href="<?php echo $link; ?>">
-<?php echo JHTML::_('image', 'components/com_thm_organizer/assets/images/'.$image, $text ); ?>
-                    <span><?php echo $text; ?></span>
-                </a>
-            </div>
-        </div>
-<?php
-    }
 }
-?>
