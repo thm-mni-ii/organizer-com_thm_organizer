5f94612ea13e2ddf4819fdcde157acb2861e8ac1
J.Antrim <J.Antrim@798ade6b-684f-418b-bd88-1ba854bd16d0>
2011-02-03 15:35:05 +0000

    renamed for standardization
    git-svn-id: http://tracking.mni.fh-giessen.de/svn/joomla/projects/com_thm_organizer/trunk@3098 798ade6b-684f-418b-bd88-1ba854bd16d0

---
+++ admin/views/virtual_schedule_edit/tmpl/js/hideshowmultiselect.js
@@ -0,0 +1,42 @@
+
+function setRessource()
+{
+	var types = Ext.get('vscheduler_types');
+	var classes = Ext.get('vscheduler_classes');
+	var rooms = Ext.get('vscheduler_rooms');
+	var teachers = Ext.get('vscheduler_teachers');
+
+	var roomDepartments = Ext.get('vscheduler_roomDepartments');
+	var teacherDepartments = Ext.get('vscheduler_teacherDepartments');
+	var classesDepartments = Ext.get('vscheduler_classesDepartments');
+
+	classes.dom.parentNode.style.display = "none";
+	rooms.dom.parentNode.style.display = "none";
+	teachers.dom.parentNode.style.display = "none";
+
+	roomDepartments.dom.parentNode.style.display = "none";
+	teacherDepartments.dom.parentNode.style.display = "none";
+	classesDepartments.dom.parentNode.style.display = "none";
+
+	if(types.getValue() == "class")
+	{
+		classes.dom.parentNode.style.display = "block";
+		classesDepartments.dom.parentNode.style.display = "block";
+	}
+	else
+	if(types.getValue() == "room")
+	{
+		rooms.dom.parentNode.style.display = "block";
+		roomDepartments.dom.parentNode.style.display = "block";
+	}
+	else
+	if(types.getValue() == "teacher")
+	{
+		teachers.dom.parentNode.style.display = "block";
+		teacherDepartments.dom.parentNode.style.display = "block";
+	}
+}
+
+Ext.onReady(function() {
+	setRessource();
+});
