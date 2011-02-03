38be7d8601ee7361225bc927ae5ed8e9e9e6f3c8
J.Antrim <J.Antrim@798ade6b-684f-418b-bd88-1ba854bd16d0>
2011-02-03 15:42:26 +0000

    mostly a rename, however several links were modified to reflect changes to file names
    git-svn-id: http://tracking.mni.fh-giessen.de/svn/joomla/projects/com_thm_organizer/trunk@3102 798ade6b-684f-418b-bd88-1ba854bd16d0

---
+++ admin/models/monitor_manager.php
@@ -0,0 +1,46 @@
+<?php
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+ 
+jimport( 'joomla.application.component.model' );
+ 
+/**
+ * Room IP List Model
+ *
+ * @package    Giessen Scheduler
+ * @subpackage Components
+ */
+class thm_organizersModelmonitor_manager extends JModel
+{
+    public $monitors = null;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->monitors = array();
+        $this->loadMonitors();
+        if(count($this->monitors) > 0) $this->setMonitorEditLinks();
+    }
+
+    private function loadMonitors()
+    {
+        $dbo = JFactory::getDBO();
+        $query = $dbo->getQuery(true);
+        $query->select('*');
+        $query->from('#__thm_organizer_monitors AS m');
+        $query->leftJoin('#__thm_organizer_semester AS s ON m.sid = s.sid');
+        $dbo->setQuery((string)$query);
+
+        $monitors = $dbo->loadAssocList();
+        $this->monitors = $monitors;
+    }
+
+    private function setMonitorEditLinks()
+    {
+        foreach($this->monitors as $mKey => $mValue)
+        {
+            $this->monitors[$mKey]['link'] = 'index.php?option=com_thm_organizer&view=monitor_edit&monitorID='.$mValue['monitorID'];
+        }
+    }
+}
