08342240965fb9c7bc3603a75a366bf5819ab1a3
d.grabbert <d.grabbert@798ade6b-684f-418b-bd88-1ba854bd16d0>
2011-02-18 12:52:21 +0000

    Category Manager kann im Backend aufgerufen werden.
    git-svn-id: http://tracking.mni.fh-giessen.de/svn/joomla/projects/com_thm_organizer/trunk@3256 798ade6b-684f-418b-bd88-1ba854bd16d0

--- admin/views/category_manager/tmpl/default.php
+++ admin/views/category_manager/tmpl/default.php
@@ -1,56 +1,45 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
-<form action="index.php"  name="adminForm" method="post" >
-<div  id="editcell" style="width:100%;">
-    <table class="adminlist">
-    <thead>
-        <tr>
-        	<th width="5" />
-            <th width="10">
-                <?php echo JText::_( 'Kategorie' ); ?>
-            </th>
-            <th>
-                <?php echo JText::_( 'Beschreibung' ); ?>
-            </th>
-            <th width="50px"></th>
-        </tr>            
-    </thead>
-    <?php
-    $k = 0;
-    for ($i=0, $n=count( $this->items ); $i < $n; $i++)
-    {
-        $row =& $this->items[$i];
-        $checked = JHTML::_( 'grid.id', $i, $row->ecid );
-        $link = JRoute::_('index.php?option=com_thm_organizer&controller=category&task=edit&cid[]='. $row->ecid ); 
-        ?>
-        <tr class="<?php echo "row$k"; ?>">
-            <td align="center">
-                <?php echo $checked; ?>
-            </td>
-            <td align="center">
-	            <a href='<?php echo $link; ?>'><?php echo $row->ecname; ?></a>
-            </td>
-            <td align="center">
-                <?php echo $row->ecdescription; ?>
-            </td>
-            <td align="center">
-                <?php 
-                if($row->ecimage)
-                {
-                	echo '<image src="../images/thm_organizer/categories/'.$row->ecimage.'" width="100px" height="100px"/>';
-                }
-                ?>
-            </td>
-        </tr>
-        <?php
-        $k = 1 - $k;
-    }
-    ?>
-    </table>
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_thm_organizer
+ * @name        monitor manager default template
+ * @description standard template for the display of registered monitors
+ * @author      James Antrim jamesDOTantrimATyahooDOTcom
+ * @copyright   TH Mittelhessen 2011
+ * @license     GNU GPL v.2
+ * @link        www.mni.fh-giessen.de
+ * @version     0.0.1
+ */
+defined('_JEXEC') or die('Restricted access');?>
+<div id="thm_organizer_mm" >
+    <form action="<?php echo JRoute::_('index.php?option=com_thm_organizer'); ?>" method="post" name="adminForm">
+        <div id="editcell">
+            <table class="adminlist thm_organizer_mm_table">
+                <thead>
+                    <tr>
+                        <th />
+                        <th><?php echo JText::_( 'ID'); ?></th>
+                        <th><?php echo JText::_( 'Kategorie' ); ?></th>
+                        <th><?php echo JText::_( 'Beschreibung' ); ?></th>
+                    </tr>
+                </thead>
+<?php $k = 0; if(!empty($this->categories)): foreach($this->categories as $category) :
+        $checked = JHTML::_( 'grid.id', $k, $category['id'] );
+        $class = ($k % 2 == 0)?  'row0' : 'row1';
+        $linkstart = "<a href='".$category['link']."' >";
+        $linkend = "</a>";
+        $k++ ?>
+	        <tr class="<?php echo $class; ?>">
+	            <td class="thm_organizer_mm_checkbox"><?php echo $checked; ?></td>
+	            <td class="thm_organizer_mm_ip"><?php echo $category['id']; ?></td>
+	            <td class="thm_organizer_mm_ip"><?php echo $linkstart.$category['globaldisplay'].$linkend; ?></td>
+	            <td class="thm_organizer_mm_room"><?php echo $linkstart.$category['reservesobjects'].$linkend; ?></td>
+	        </tr>
+<?php endforeach; endif;?>
+	    </table>
+	</div>
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="boxchecked" value="0" />
+        <?php echo JHtml::_('form.token'); ?>
+    </form>
 </div>
- 
-<input type="hidden" name="option" value="com_thm_organizer" />
-<input type="hidden" name="task" value="" />
-<input type="hidden" name="boxchecked" value="0" />
-<input type="hidden" name="controller" value="category" />
- 
-</form>
--- admin/views/category_manager/view.html.php
+++ admin/views/category_manager/view.html.php
@@ -1,132 +1,40 @@
-<?php
+<?php 
+defined('_JEXEC') or die('Restricted Access');
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+jimport('joomla.application.component.view');
+require_once JPATH_COMPONENT.'/assets/helpers/thm_organizerHelper.php';
 
-jimport( 'joomla.application.component.view');
-
-/**
- * View class for the  Giessen Scheduler Monitors screen
- *
- * @package Joomla
- * @subpackage  Giessen Scheduler
- */
-class  thm_organizersViewcategory_manager extends JView
-{   
-    protected $items;
-    protected $pagination;
-
-    function display($tpl = null)
-    {
-        $this->items		= $this->get('Items');
-        $this->pagination	= $this->get('Pagination');
-        $this->addToolbar();
-        parent::display($tpl);
-    }
-
-    /**
-     * Add the page title and toolbar.
-     *
-     * @since	1.6
-     */
-    protected function addToolbar()
-    {
-
-
-//        JToolBarHelper::title( JText::_( 'THM - Organizer: Category Manager' ), 'generic.png' );
-//        JToolBarHelper::addNewX();
-//        JToolBarHelper::editListX();
-//        JToolBarHelper::deleteList();
-//
-//        //Create Submenu
-//        $model = $this->getModel();
-//        foreach($model->data->links as $link)
-//        {
-//            JSubMenuHelper::addEntry( JText::_( $link['name'] ), 'index.php?'.$link['link']);
-//        }
-		// Initialise variables.
-		$categoryId	= $this->state->get('filter.category_id');
-		$component	= $this->state->get('filter.component');
-		$section	= $this->state->get('filter.section');
-		$canDo		= null;
-
-
- 		// Load the category helper.
-		require_once JPATH_COMPONENT.'/helpers/categories.php';
-
-		// Get the results for each action.
-		$canDo = CategoriesHelper::getActions($component, $categoryId);
-
-		// If a component categories title string is present, let's use it.
-		if ($lang->hasKey($component_title_key = strtoupper($component.($section?"_$section":'')).'_CATEGORIES_TITLE')) {
-			$title = JText::_($component_title_key);
-		}
-		// Else if the component section string exits, let's use it
-		elseif ($lang->hasKey($component_section_key = strtoupper($component.($section?"_$section":'')))) {
-			$title = JText::sprintf( 'COM_CATEGORIES_CATEGORIES_TITLE', $this->escape(JText::_($component_section_key)));
-		}
-		// Else use the base title
-		else {
-			$title = JText::_('COM_CATEGORIES_CATEGORIES_BASE_TITLE');
-		}
-
-		// Load specific css component
-		JHtml::_('stylesheet',$component.'/administrator/categories.css', array(), true);
-
-		// Prepare the toolbar.
-		JToolBarHelper::title($title, 'categories '.substr($component,4).($section?"-$section":'').'-categories');
-
-		if ($canDo->get('core.create')) {
-			 JToolBarHelper::custom('category.add', 'new.png', 'new_f2.png', 'JTOOLBAR_NEW', false);
-		}
-
-		if ($canDo->get('core.edit' ) || $canDo->get('core.edit.own')) {
-			JToolBarHelper::custom('category.edit', 'edit.png', 'edit_f2.png', 'JTOOLBAR_EDIT', true);
-			JToolBarHelper::divider();
-		}
-
-		if ($canDo->get('core.edit.state')) {
-			JToolBarHelper::custom('categories.publish', 'publish.png', 'publish_f2.png', 'JTOOLBAR_PUBLISH', true);
-			JToolBarHelper::custom('categories.unpublish', 'unpublish.png', 'unpublish_f2.png', 'JTOOLBAR_UNPUBLISH', true);
-			JToolBarHelper::divider();
-			JToolBarHelper::archiveList('categories.archive','JTOOLBAR_ARCHIVE');
-		}
-
-		if (JFactory::getUser()->authorise('core.admin')) {
-			JToolBarHelper::custom('categories.checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
-		}
-
-		if ($this->state->get('filter.published') == -2 && $canDo->get('core.delete', $component)) {
-			JToolBarHelper::deleteList('', 'categories.delete','JTOOLBAR_EMPTY_TRASH');
-		}
-		else if ($canDo->get('core.edit.state')) {
-			JToolBarHelper::trash('categories.trash','JTOOLBAR_TRASH');
-			JToolBarHelper::divider();
-		}
-
-		if ($canDo->get('core.admin')) {
-			JToolBarHelper::custom('categories.rebuild', 'refresh.png', 'refresh_f2.png', 'JTOOLBAR_REBUILD', false);
-			JToolBarHelper::preferences($component);
-			JToolBarHelper::divider();
-		}
-
-		// Compute the ref_key if it does exist in the component
-		if (!$lang->hasKey($ref_key = strtoupper($component.($section?"_$section":'')).'_CATEGORIES_HELP_KEY')) {
-			$ref_key = 'JHELP_COMPONENTS_'.strtoupper(substr($component,4).($section?"_$section":'')).'_CATEGORIES';
-		}
-
-		// Get help for the categories view for the component by
-		// -remotely searching in a language defined dedicated URL: *component*_HELP_URL
-		// -locally  searching in a component help file if helpURL param exists in the component and is set to ''
-		// -remotely searching in a component URL if helpURL param exists in the component and is NOT set to ''
-		if ($lang->hasKey($lang_help_url = strtoupper($component).'_HELP_URL')) {
-			$debug = $lang->setDebug(false);
-			$url = JText::_($lang_help_url);
-			$lang->setDebug($debug);
-		}
-		else {
-			$url = null;
-		}
-		JToolBarHelper::help($ref_key, JComponentHelper::getParams( $component )->exists('helpURL'), $url);
-	}
-}
+class thm_organizersViewCategory_manager extends JView {
 	
+	public function display($tpl = null){
+		$document = JFactory::getDocument();
+		$document->addStyleSheet($this->baseurl."/components/com_thm_organizer/assets/css/thm_organizer.css");
+		
+		$this->addToolBar();
+		thm_organizerHelper::addSubmenu('category_manager');
+		
+		$model = $this->getModel();
+		$categories = $model->categories;
+        $this->assignRef( 'categories', $categories );
+        
+        
+        
+        parent::display($tpl);
+	}
+	
+	private function addToolBar(){
+	 JToolBarHelper::title( JText::_( 'Category Manager' ), 'generic.png' );
+        $allowedActions = thm_organizerHelper::getActions('category_manager');
+        if($allowedActions->get("core.admin") or $allowedActions->get("core.manage"))
+        {
+            if($allowedActions->get("core.admin") or $allowedActions->get("core.create"))
+                    JToolBarHelper::addNew( 'category.new' );
+            if($allowedActions->get("core.admin") or $allowedActions->get("core.edit"))
+                    JToolBarHelper::editList('category.edit');
+            if($allowedActions->get("core.admin") or $allowedActions->get("core.delete"))
+                    JToolBarHelper::deleteList( JText::_('Are you sure you wish to delete the marked entries?'), 'category.delete');
+        }
+	}
+	
+	
+}
