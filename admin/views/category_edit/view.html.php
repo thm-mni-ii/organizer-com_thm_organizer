70d73a626215794f59f331f6749a456ceb8b92d3
d.grabbert <d.grabbert@798ade6b-684f-418b-bd88-1ba854bd16d0>
2011-02-18 13:22:02 +0000

    git-svn-id: http://tracking.mni.fh-giessen.de/svn/joomla/projects/com_thm_organizer/trunk@3262 798ade6b-684f-418b-bd88-1ba854bd16d0

--- admin/views/category_edit/view.html.php
+++ admin/views/category_edit/view.html.php
@@ -1,27 +1,77 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_thm_organizer
+ * @name        monitor editor view
+ * @author      James Antrim jamesDOTantrimATyahooDOTcom
+ * @copyright   TH Mittelhessen <year>
+ * @license     GNU GPL v.2
+ * @link        www.mni.fh-giessen.de
+ * @version     0.0.1
+ */
+
 defined('_JEXEC') or die( 'Restricted access' );
 jimport( 'joomla.application.component.view' );
-class thm_organizersViewCategory extends JView
+require_once JPATH_COMPONENT.'/assets/helpers/thm_organizerHelper.php';
+
+class thm_organizersViewcategory_edit extends JView
 {
-	function display($tpl = null)
-	{
-		$category =& $this->get('Data');
-		$isNew = ($category->ecid == 0);
-		$text = $isNew ? JText::_( 'Neu' ) : JText::_( 'Edit' );
-		JToolBarHelper::title( 'Kategorie: <small> [ '.$text.' ]</small>' );
-		JToolBarHelper::save();
-		if($isNew) JToolBarHelper::cancel();
-		else JToolBarHelper::cancel( 'cancel', 'Close' );
-	 	$this->assignRef('category', $category);
-	 	$model = $this->getModel();
-	 	$usergroups = $model->getUserGroups();
-		$this->assignRef('usergroups', JHTML::_('select.genericlist', $usergroups, 'access','size="1" class="inputbox"', 'id', 'name', $category->access ));
-		
-	 	$javascript = "onchange=\"javascript:if (document.forms[0].image.options[selectedIndex].value!='') {document.imagelib.src='../images/thm_organizer/categories/' + document.forms[0].image.options[selectedIndex].value} else {document.imagelib.src='../images/blank.png'}\"";
-		$imagelist = JHTML::_('list.images', 'image', $category->ecimage, $javascript, '/images/thm_organizer/categories/' );
-	 	$this->assignRef('imagelist', $imagelist);
-	 	parent::display($tpl);
-	}
+    function display($tpl = null)
+    {
+        $model = $this->getModel();
+        $document = & JFactory::getDocument();
+        $document->addStyleSheet($this->baseurl."/components/com_thm_organizer/assets/css/thm_organizer.css");
+
+        $categoryID = $model->categoryID;
+        $this->assignRef( 'categoryID', $categoryID );
+
+        $ip = $model->ip;
+        $this->assignRef( 'ip', $ip );
+
+        $room = $model->room;
+        $rooms = $model->rooms;
+        if(!empty($rooms))
+        {
+            $roombox = JHTML::_('select.genericlist', $rooms, 'room', 'id="thm_organizer_me_roombox" class="thm_organizer_me_rsemesterbox" size="1"', 'id', 'name', $room);
+            $this->assignRef('roombox', $roombox);
+        }
+
+        $sid = $model->sid;
+        $semesters = $model->semesters;
+        if(!empty($semesters))
+        {
+            $semesterbox =  JHTML::_('select.genericlist', $semesters, 'semester', 'id="thm_organizer_me_rsemesterbox" class="thm_organizer_me_rsemesterbox" size="1"', 'sid', 'name', $sid);
+            $this->assignRef('semesterbox', $semesterbox);
+        }
+
+        $isNew = ($categoryID == 0)? true : false;
+        $allowedActions = thm_organizerHelper::getActions('category_edit');
+        if($allowedActions->get("core.admin") or $allowedActions->get("core.manage"))
+            $this->addToolBar($allowedActions, $isNew);
+
+        parent::display($tpl);
+    }
+
+    private function addToolBar($allowedActions, $isNew = true)
+    {
+        $canSave = false;
+        if($isNew)
+        {
+            $titleText = JText::_( 'category Manager: Add a New category' );
+            if($allowedActions->get("core.create") or $allowedActions->get("core.edit"))
+                    $canSave = true;
+        }
+        else
+        {
+            $titleText = JText::_( 'category Manager: Edit an Existing category' );
+            if($allowedActions->get("core.edit")) $canSave = true;
+        }
+        JToolBarHelper::title( $titleText, 'generic.png' );
+        if($canSave) JToolBarHelper::save('category.save', 'JTOOLBAR_SAVE');
+        if($allowedActions->get("core.create"))
+            JToolBarHelper::custom('category.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
+        if($isNew) JToolBarHelper::cancel('category.cancel', 'JTOOLBAR_CANCEL');
+        else JToolBarHelper::cancel( 'category.cancel', 'JTOOLBAR_CANCEL');
+    }
 }
-?>
 	
