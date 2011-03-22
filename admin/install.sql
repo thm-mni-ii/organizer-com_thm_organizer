1c0c5e5d4fefcf5c43890c53ead9a23c80724f17
W.Rost <W.Rost@798ade6b-684f-418b-bd88-1ba854bd16d0>
2011-03-22 12:42:06 +0000

    - change plantypes
    git-svn-id: http://tracking.mni.fh-giessen.de/svn/joomla/projects/com_thm_organizer/trunk@3652 798ade6b-684f-418b-bd88-1ba854bd16d0

--- admin/install.sql
+++ admin/install.sql
@@ -113,8 +113,8 @@ CREATE TABLE IF NOT EXISTS `#__thm_organizer_plantype` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT IGNORE INTO `#__thm_organizer_plantype` (`id`, `plantype`) VALUES
-(1, 'GP-Untis Schedules'),
-(2, 'Curricula');
+(1, 'Stundenplan'),
+(2, 'Lehrplan');
 
 CREATE TABLE IF NOT EXISTS `#__thm_organizer_lesson_times` (
   `lessonID` int(11) NOT NULL,
