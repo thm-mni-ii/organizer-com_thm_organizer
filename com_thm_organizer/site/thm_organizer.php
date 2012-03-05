20413455dcb0aa5e9fd9ac97ab49a8c8542f2400
James Antrim <james.antrim@mni.thm.de>
2012-03-05 21:51:23 +0100

    minor changes to main controller

--- com_thm_organizer/site/thm_organizer.php
+++ com_thm_organizer/site/thm_organizer.php
@@ -3,33 +3,27 @@
  * @package     Joomla.Site
  * @subpackage  com_thm_organizer
  * @author      James Antrim jamesDOTantrimATyahooDOTcom
- * @copyright   TH Mittelhessen 2011
+ * @copyright   TH Mittelhessen 2012
  * @license     GNU GPL v.2
- * @link        www.mni.fh-giessen.de
- * @version     0.011
+ * @link        www.mni.thm.de
+ * @version     2.5
  */
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die;
 jimport('joomla.application.component.controller');
 require_once( JPATH_COMPONENT.DS.'controller.php' );
 
-$controller = "";
+$controller = '';
 $handler = explode(".", JRequest::getVar('task'));
 if(!empty($handler))
 {
-    if(count($handler) == 2)
-    {
-        $controller = $handler[0];
-        $task = $handler[1];
-    }
-    else
-       $task = JRequest::getVar('task');
+    if(count($handler) == 2) list($controller, $task) = $handler;
+    else $task = JRequest::getVar('task');
 }
-if(!empty($controller))
+if($controller !== '')
 {
     $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
-    if (file_exists($path)) require_once $path;
-    else $controller = '';
+    file_exists($path)? require_once $path : $controller = '';
 }
 $classname = 'thm_organizerController'.$controller;
 $controller = new $classname();
