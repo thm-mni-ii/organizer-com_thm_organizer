748f1f8c088187a6d4ad6d4fdd761c717b4d28c7
Markus Bader <markus.bader@mni.thm.de>
2012-03-07 12:13:27 +0100

    teacher manager added and ready for testing

---
+++ com_thm_organizer/admin/views/teacher_manager/view.html.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_thm_organizer
+ * @name        schedule manager view
+ * @description provides a list of schedules
+ * @author      James Antrim jamesDOTantrimATyahooDOTcom
+ * @copyright   TH Mittelhessen 2011
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
+ * @version     1.7.0
+ */
+defined('_JEXEC') or die( 'Restricted access' );
+jimport( 'joomla.application.component.view' );
+require_once JPATH_COMPONENT.'/assets/helpers/thm_organizerHelper.php';
+
+class thm_organizersViewteacher_manager extends JView
+{
+    protected $pagination;
+    protected $state;
+    protected $subsubbar;
+    
+    // variable for filters
+    protected $departments;
+    protected $campuses;
+    protected $institutions;
+
+    function display($tpl = null)
+    {
+        if(!JFactory::getUser()->authorise('core.admin'))
+            return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+
+        JHtml::_('behavior.tooltip');
+        JHtml::_('behavior.multiselect');
+
+        $model = $this->getModel();
+        $document = JFactory::getDocument();
+        $document->addStyleSheet($this->baseurl."/components/com_thm_organizer/assets/css/thm_organizer.css");
+
+        $this->teachers 	= $this->get('Items');
+        $this->pagination 	= $this->get('Pagination');
+        $this->state 		= $this->get('State');
+        $this->departments 	= $model->departments;
+        $this->campuses		= $model->campuses;
+        $this->institutions	= $model->institutions;
+        
+        // for sorting
+        $state = $this->get('State');
+        $this->orderby   = $state->get('filter_order');
+        $this->direction = $state->get('filter_order_Dir');
+        
+        $this->addToolBar();
+        if (count($this->teachers)) $this->addLinks();
+
+        parent::display($tpl);
+    }
+
+    /**
+     * addLinks
+     *
+     * creates links to the edit view for the particular schedule
+     */
+    private function addLinks()
+    {
+        $editURL = 'index.php?option=com_thm_organizer&view=teacher_edit&teacherID=';
+        foreach($this->teachers as $key => $room)
+            $this->teachers[$key]->url = $editURL.$room->id;
+    }
+
+    /**
+     * addToolBar
+     *
+     * creates a joomla administrative tool bar
+     */
+    private function addToolBar()
+    {
+        $title = JText::_( 'COM_THM_ORGANIZER_TM_TITLE' );
+        JToolBarHelper::title($title);
+        JToolBarHelper::addNew('teacher.add');
+        JToolBarHelper::editList('teacher.edit');
+        JToolBarHelper::deleteList(JText::_( 'COM_THM_ORGANIZER_RMM_DELETE_CONFIRM'),'teacher.delete');
+
+    }
+}
