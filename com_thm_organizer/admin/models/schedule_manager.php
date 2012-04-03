eb9383fe154244ce1380fad68101dff1cb73cb8f
James Antrim <james.antrim@mni.thm.de>
2012-04-03 13:43:34 +0200

    #2518

--- com_thm_organizer/admin/models/schedule_manager.php
+++ com_thm_organizer/admin/models/schedule_manager.php
@@ -87,7 +87,7 @@ class thm_organizersModelschedule_manager extends JModelList
         $state = $this->getUserStateFromRequest($this->context.'.filter.state', 'filter_state');
         $this->setState('filter.state', $state);
 
-        $selectedSemester = JRequest::getInt('filter_semester');
+        $selectedSemester = JRequest::getVar('filter_semester');
         $referredSemester = JRequest::getInt('semesterID');
         $stateSemester = $this->getUserStateFromRequest($this->context.'.filter.semester', 'filter_semester');
         if($selectedSemester)$semesterID = $selectedSemester;
