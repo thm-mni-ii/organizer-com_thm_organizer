1b0debd535cf1a552857ec151250131da38c86d9
James Antrim <james.antrim@mni.thm.de>
2012-09-18 15:36:23 +0200

    drastic changes because of the migration to untis 2013, and merge with com_thm_curriculum

--- com_thm_organizer/admin/sql/updates/mysql/0.0.1.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `#__thm_organizer_rooms` ADD `campus`  varchar(128) NOT NULL DEFAULT '' AFTER `manager`;
-ALTER TABLE `#__thm_organizer_rooms` ADD `building` varchar(64) NOT NULL DEFAULT '' AFTER `campus`;
-ALTER TABLE `#__thm_organizer_rooms` ADD `floor` int(2) NOT NULL DEFAULT 0 AFTER `building`;
-
-ALTER TABLE `#__thm_organizer_classes` DROP INDEX `teacherID`;
-ALTER TABLE `#__thm_organizer_classes` DROP `teacherID`;
-ALTER TABLE `#__thm_organizer_classes` ADD `manager` varchar(20) AFTER `alias`;
-
