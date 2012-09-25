6811f6ef92a6de8dc01aad3b680ba4484dd2859f
James Antrim <james.antrim@mni.thm.de>
2012-09-25 17:34:50 +0200

    brought file into coding standards eliminated warnings due to inconsistency between the display function and its parent function

--- com_thm_organizer/admin/controller.php
+++ com_thm_organizer/admin/controller.php
@@ -1,28 +1,45 @@
 <?php
 /**
- * @package     Joomla.Administrator
- * @subpackage  com_thm_organizer
- * @name        controller thm_organizer
- * @description the entry file for the administrative area of thm_organizer
- *              accepts the controller/task parameters and redirects to specific
- *              controllers
- * @author      James Antrim jamesDOTantrimATmniDOTthmDOTde
- * @copyright   TH Mittelhessen 2012
- * @license     GNU GPL v.2
- * @link        www.mni.thm.de
- * @version     2.5
+ *@category    component
+ * 
+ *@package     THM_Organizer
+ * 
+ *@subpackage  com_thm_organizer
+ *@name        main controller class for thm organizer admin area
+ *@author      James Antrim jamesDOTantrimATmniDOTthmDOTde
+ * 
+ *@copyright   2012 TH Mittelhessen
+ * 
+ *@license     GNU GPL v.2
+ *@link        www.mni.thm.de
+ *@version     0.1.0
  */
-
-defined( '_JEXEC' ) or die( 'Restricted access' );
-
+defined('_JEXEC') or die;
 jimport('joomla.application.component.controller');
-require_once JPATH_COMPONENT.'/assets/helpers/thm_organizerHelper.php';
-
+require_once JPATH_COMPONENT . '/assets/helpers/thm_organizerHelper.php';
+/**
+ * Class for non-specific component calls 
+ * 
+ * @package  Admin
+ * 
+ * @since    2.5.4
+ *  */
 class thm_organizersController extends JController
 {
-    public function display()
+    /**
+     * Typical view method for MVC based architecture
+     *
+     * This function is provide as a default implementation, in most cases
+     * you will need to override it in your own controllers.
+     *
+     * @param   boolean  $cachable   If true, the view output will be cached
+     * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+     *
+     * @return  JController  A JController object to support chaining.
+     */
+    public function display($cachable = false, $urlparams = false)
     {
         thm_organizerHelper::addSubmenu(JRequest::getCmd('view'));
-        parent::display();
+        parent::display($cachable, $urlparams);
     }
 }
