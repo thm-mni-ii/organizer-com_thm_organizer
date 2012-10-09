8417061b595d1fc806beea85308631fc1f20fa6f
Wolf Rost <wolf.rost@mni.thm.de>
2012-10-09 11:28:22 +0200

    -beautified code (http://jsbeautifier.org/)

--- com_thm_organizer/site/views/scheduler/tmpl/mySched/advancedFunctions.js
+++ com_thm_organizer/site/views/scheduler/tmpl/mySched/advancedFunctions.js
@@ -1,73 +1,88 @@
 var oldMainToolbar = MySched.layout.getMainToolbar;
 
-MySched.layout.getMainToolbar = function() {
-	var btnEvent = Ext.create('Ext.Button', {
-		// Event anlegen
-		text : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_EVENT_CREATE,
-		id : 'btnEvent',
-		hidden : false,
-		iconCls : 'tbEvent',
-		handler : addNewEvent
-	});
-	var ToolbarObjects = oldMainToolbar();
-	var newMainToolbar = ToolbarObjects.AddTo(3, btnEvent);
-	return newMainToolbar;
+MySched.layout.getMainToolbar = function ()
+{
+    var btnEvent = Ext.create('Ext.Button',
+    {
+        // Event anlegen
+        text: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_EVENT_CREATE,
+        id: 'btnEvent',
+        hidden: false,
+        iconCls: 'tbEvent',
+        handler: addNewEvent
+    });
+    var ToolbarObjects = oldMainToolbar();
+    var newMainToolbar = ToolbarObjects.AddTo(3, btnEvent);
+    return newMainToolbar;
 };
 
 var addEvent = {
-	text : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_EVENT_CREATE,
-	icon : MySched.mainPath + "images/calendar_add.png",
-	handler : function() {
-		addNewEvent(null, MySched.BlockMenu.day, MySched.BlockMenu.stime,
-				MySched.BlockMenu.etime);
-	},
-	xtype : "button"
+    text: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_EVENT_CREATE,
+    icon: MySched.mainPath + "images/calendar_add.png",
+    handler: function ()
+    {
+        addNewEvent(null, MySched.BlockMenu.day, MySched.BlockMenu.stime,
+        MySched.BlockMenu.etime);
+    },
+    xtype: "button"
 }
 
 MySched.BlockMenu.Menu[MySched.BlockMenu.Menu.length] = addEvent;
 
-window.onbeforeunload = function() {
-	if (typeof MySched.layout.tabpanel === "undefined")
-		return;
-	var tabs = MySched.layout.tabpanel.items.items;
-	var temptabs = tabs;
-	var check = false;
-	var tosave = false;
-	var i = 0;
-	var ti = 0;
+window.onbeforeunload = function ()
+{
+    if (typeof MySched.layout.tabpanel === "undefined") return;
+    var tabs = MySched.layout.tabpanel.items.items;
+    var temptabs = tabs;
+    var check = false;
+    var tosave = false;
+    var i = 0;
+    var ti = 0;
 
-	for (i = 0; i < tabs.length; i++) {
-		if (tabs[i].mSchedule.status === "unsaved") {
-			check = confirm(MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_MYSCHEDULER_CHANGED);
-			if (check === true) {
-				for (ti = 0; ti < temptabs.length; ti++) {
-					if (temptabs[ti].mSchedule.status === "unsaved") {
-						if (temptabs[ti].mSchedule.id === "mySchedule") {
-							temptabs[ti].mSchedule.save(_C('ajaxHandler'),
-									false, "UserSchedule.save");
-						} else {
-							temptabs[ti].mSchedule.save(_C('ajaxHandler'),
-									false, "saveScheduleChanges");
-						}
-						tosave = true;
-					}
-				}
-				break;
-			} else {
-				break;
-			}
-		}
-	}
+    for (i = 0; i < tabs.length; i++)
+    {
+        if (tabs[i].mSchedule.status === "unsaved")
+        {
+            check = confirm(MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_MYSCHEDULER_CHANGED);
+            if (check === true)
+            {
+                for (ti = 0; ti < temptabs.length; ti++)
+                {
+                    if (temptabs[ti].mSchedule.status === "unsaved")
+                    {
+                        if (temptabs[ti].mSchedule.id === "mySchedule")
+                        {
+                            temptabs[ti].mSchedule.save(_C('ajaxHandler'),
+                            false, "UserSchedule.save");
+                        }
+                        else
+                        {
+                            temptabs[ti].mSchedule.save(_C('ajaxHandler'),
+                            false, "saveScheduleChanges");
+                        }
+                        tosave = true;
+                    }
+                }
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 
-	if (check === true && tosave === true) {
-		var jetzt = new Date();
-		var sek = jetzt.getSeconds();
-		var undjetzt = new Date();
-		var undsek = undjetzt.getSeconds();
-		sek = sek + 3;
-		while (sek % 60 > undsek) {
-			undjetzt = new Date();
-			undsek = undjetzt.getSeconds();
-		}
-	}
+    if (check === true && tosave === true)
+    {
+        var jetzt = new Date();
+        var sek = jetzt.getSeconds();
+        var undjetzt = new Date();
+        var undsek = undjetzt.getSeconds();
+        sek = sek + 3;
+        while (sek % 60 > undsek)
+        {
+            undjetzt = new Date();
+            undsek = undjetzt.getSeconds();
+        }
+    }
 };
--- com_thm_organizer/site/views/scheduler/tmpl/mySched/mapping.js
+++ com_thm_organizer/site/views/scheduler/tmpl/mySched/mapping.js
@@ -2,73 +2,86 @@
  * Mapping von Kuerzeln auf Namen
  * @author thorsten
  */
-
 /**
  * Ersaetzt Ids zu Namen Bei Teacherenten, Studiengaengen, Modulen etc.
  */
-MySched.Mapping = function() {
-	var teacher, module, subject, lecture, room, types, roomtype, degree, field;
+MySched.Mapping = function ()
+{
+    var teacher, module, subject, lecture, room, types, roomtype, degree, field;
 
-	return {
-		init : function() {
-			this.teacher = new MySched.Collection();
-			this.module = new MySched.Collection();
-			this.subject = new MySched.Collection();
-			this.room = new MySched.Collection();
-			this.roomtype = new MySched.Collection();
-			this.degree = new MySched.Collection();
-			this.field = new MySched.Collection();
-			this.types = {
-				teacher : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_TEACHER,
-				module : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_SEMESTER,
-				room : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_ROOM,
-				subject : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_SUBJECTS,
-				roomtype : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_ROOMTYPE,
-				degree : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_DEGREE,
-				field : MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_FIELD
-			};
+    return {
+        init: function ()
+        {
+            this.teacher = new MySched.Collection();
+            this.module = new MySched.Collection();
+            this.subject = new MySched.Collection();
+            this.room = new MySched.Collection();
+            this.roomtype = new MySched.Collection();
+            this.degree = new MySched.Collection();
+            this.field = new MySched.Collection();
+            this.types = {
+                teacher: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_TEACHER,
+                module: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_SEMESTER,
+                room: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_ROOM,
+                subject: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_SUBJECTS,
+                roomtype: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_ROOMTYPE,
+                degree: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_DEGREE,
+                field: MySchedLanguage.COM_THM_ORGANIZER_SCHEDULER_FIELD
+            };
 
-		},
-		def : function(arr, val, def) {
-			if (arr && arr[val])
-				return arr[val];
-			return def || '';
-		},
-		getName : function(type, id) {
-			return this.def(this[type].get(id, id), 'surname', id);
-		},
-		getObjectField : function(type, id, field) {
-			return this.def(this[type].get(id, id), field, id);
-		},
-		getObjects : function(type, id) {
-			return this.def(this[type].get(id, id), 'objects', id);
-		},
-		getObject : function(type, id) {
-			return this[type].get(id);
-		},
-		getTeacherSurname : function(id) {
-			return this.def(this.teacher.get(id, id), 'surname', id);
-		},
-		getModuleName : function(id) {
-			return this.def(this.module.get(id, id), 'name', id);
-		},
-		getModuleFullName : function(id) {
-			return this.def(this.module.get(id, id), 'restriction', id);
-		},
-		getRoomName : function(id) {
-			return this.def(this.room.get(id, id), 'name', id);
-		},
-		getSubjectName : function(id) {
-			return this.def(this.subject.get(id, id), 'longname', id);
-		},
-		getLectureName : function(id) {
-			return this.def(this.subject.get(id, id), 'name', id);
-		},
-		getLectureDescription : function(id) {
-			return this.def(this.subject.get(id, id), 'desc');
-		},
-		getFullTypeName : function(id) {
-			return this.types[id.toLowerCase()];
-		}
-	}
+        },
+        def: function (arr, val, def)
+        {
+            if (arr && arr[val]) return arr[val];
+            return def || '';
+        },
+        getName: function (type, id)
+        {
+            return this.def(this[type].get(id, id), 'surname', id);
+        },
+        getObjectField: function (type, id, field)
+        {
+            return this.def(this[type].get(id, id), field, id);
+        },
+        getObjects: function (type, id)
+        {
+            return this.def(this[type].get(id, id), 'objects', id);
+        },
+        getObject: function (type, id)
+        {
+            return this[type].get(id);
+        },
+        getTeacherSurname: function (id)
+        {
+            return this.def(this.teacher.get(id, id), 'surname', id);
+        },
+        getModuleName: function (id)
+        {
+            return this.def(this.module.get(id, id), 'name', id);
+        },
+        getModuleFullName: function (id)
+        {
+            return this.def(this.module.get(id, id), 'restriction', id);
+        },
+        getRoomName: function (id)
+        {
+            return this.def(this.room.get(id, id), 'name', id);
+        },
+        getSubjectName: function (id)
+        {
+            return this.def(this.subject.get(id, id), 'longname', id);
+        },
+        getLectureName: function (id)
+        {
+            return this.def(this.subject.get(id, id), 'name', id);
+        },
+        getLectureDescription: function (id)
+        {
+            return this.def(this.subject.get(id, id), 'desc');
+        },
+        getFullTypeName: function (id)
+        {
+            return this.types[id.toLowerCase()];
+        }
+    }
 }();
