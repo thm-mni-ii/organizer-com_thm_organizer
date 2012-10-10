3f12d2a25376b1e7f1f296edd6a03d0512fd94cf
James Antrim <james.antrim@mni.thm.de>
2012-10-10 22:53:08 +0200

    added today's schedule renamed existing real schedule for its creation date added a categories table class

---
+++ com_thm_organizer/admin/tables/categories.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *@category    component
+ * 
+ *@package     THM_Organizer
+ * 
+ *@subpackage  com_thm_organizer
+ *@name        categories table class
+ *@author      James Antrim jamesDOTantrimATmniDOTthmDOTde
+ * 
+ *@copyright   2012 TH Mittelhessen
+ * 
+ *@license     GNU GPL v.2
+ *@link        www.mni.thm.de
+ *@version     0.1.0
+ */
+defined('_JEXEC') or die;
+jimport('joomla.application.component.table');
+/**
+ * Class representing the categories table. 
+ * 
+ * @package  Admin
+ * 
+ * @since    2.5.4
+ */
+class thm_organizerTableCategories extends JTable
+{
+    /**
+     * Constructor function for the class representing the schedules table
+     * 
+     * @param   JDatabase  &$dbo  A database connector object
+     */
+    public function __construct(&$dbo)
+    {
+        parent::__construct('#__thm_organizer_categories', 'id', $dbo);
+    }
+}
