65f07d1241788628696228f2c38ae80414b2c48a
Wolf Rost <Wolf.Rost@mni.thm.de>
2013-01-28 12:49:07 +0100

    -removed code sniffer messages

--- com_thm_organizer/site/assets/classes/eventAccess.php
+++ com_thm_organizer/site/assets/classes/eventAccess.php
@@ -1,25 +1,24 @@
 <?php
 /**
- *@category    component
- * 
- *@package     THM_Organizer
- * 
- *@subpackage  com_thm_organizer
- *@name        event access
- *@author      James Antrim jamesDOTantrimATmniDOTthmDOTde
- * 
- *@copyright   2012 TH Mittelhessen
- * 
- *@license     GNU GPL v.2
- *@link        www.mni.thm.de
- *@version     0.1.0
+ * @version     v0.1.0
+  *@category    Joomla component 
+ * @package     THM_Organizer
+ * @subpackage  com_thm_organizer.site
+ * @name        eventAccess
+ * @author      James Antrim, <james.antrim@mni.thm.de>
+ * @copyright   2012 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
  */
+
 /**
  * Contains static functions to determine user access to events
  * 
- * @package  Joomla.Site
- * 
- * @since    1.5
+ * @category	Joomla.Component.Site
+ * @package     thm_organizer
+ * @subpackage  com_thm_organizer.site
+ * @link        www.mni.thm.de
+ * @since       v0.0.1
  */
 class eventAccess
 {
