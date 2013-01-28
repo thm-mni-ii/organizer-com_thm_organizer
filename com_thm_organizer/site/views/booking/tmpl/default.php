f559bcfe7c0fe37136b73fe577055032c83d95a9
Wolf Rost <Wolf.Rost@mni.thm.de>
2013-01-28 14:39:08 +0100

    -changed comment

--- com_thm_organizer/site/views/booking/tmpl/default.php
+++ com_thm_organizer/site/views/booking/tmpl/default.php
@@ -1,17 +1,13 @@
 <?php
 /**
- *@category    component
- * 
- *@package     THM_Organizer
- * 
- *@subpackage  com_thm_organizer
- *@name        booking default template
- *@author      James Antrim jamesDOTantrimATmniDOTthmDOTde
- * 
- *@copyright   2012 TH Mittelhessen
- * 
- *@license     GNU GPL v.2
- *@link        www.mni.thm.de
- *@version     0.1.0
+ * @version     v0.1.0
+ * @category    Joomla component
+ * @package     THM_Organizer
+ * @subpackage  com_thm_organizer.site
+ * @name        booking default template
+ * @author      James Antrim, <james.antrim@mni.thm.de>
+ * @copyright   2012 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
  */
 defined('_JEXEC') or die;
