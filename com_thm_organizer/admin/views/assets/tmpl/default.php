717d5c1076656e3175c5db633cac1914905e76dd
James Antrim <james.antrim@mni.thm.de>
2013-01-31 14:49:37 +0100

    brought files in line with coding standards

--- com_thm_organizer/admin/views/assets/tmpl/default.php
+++ com_thm_organizer/admin/views/assets/tmpl/default.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * @version	    v2.0.0
  * @category    Joomla component
  * @package     THM_Curriculum
  * @subpackage  com_thm_organizer.site
@@ -11,28 +10,19 @@
  * @license     GNU GPL v.2
  * @link		www.mni.thm.de
  */
-
-// No direct access to this file
 defined('_JEXEC') or die;
-
-// Load tooltip behavior
-JHtml::_('behavior.tooltip');
 ?>
-
 <form
 	action="<?php echo JRoute::_('index.php?option=com_thm_organizer&view=assets'); ?>"
 	method="post" name="adminForm" id="adminForm">
 	<table class="adminlist">
 		<thead>
-
 			<?php echo $this->loadTemplate('head'); ?>
 		</thead>
 		<tfoot>
-
 			<?php echo $this->loadTemplate('foot'); ?>
 		</tfoot>
 		<tbody>
-
 			<?php echo $this->loadTemplate('body'); ?>
 		</tbody>
 	</table>
--- com_thm_organizer/admin/views/assets/view.html.php
+++ com_thm_organizer/admin/views/assets/view.html.php
@@ -1,21 +1,18 @@
 <?php
 /**
- * @version	    v2.0.0
  * @category    Joomla component
  * @package     THM_Curriculum
  * @subpackage  com_thm_organizer.admin
  * @name		THM_OrganizerViewAssets
  * @description THM_OrganizerViewAssets component admin view
  * @author	    Markus Baier, <markus.baier@mni.thm.de>
+ * @author      James Antrim, <james.antrim@mni.thm.de>
  * @copyright   2012 TH Mittelhessen
  * @license     GNU GPL v.2
  * @link		www.mni.thm.de
  */
-
 defined('_JEXEC') or die;
-
 jimport('joomla.application.component.view');
-
 /**
  * Class THM_OrganizerViewAssets for component com_thm_organizer
  *
@@ -25,7 +22,6 @@ jimport('joomla.application.component.view');
  * @package     thm_curriculum
  * @subpackage  com_thm_organizer.admin
  * @link        www.mni.thm.de
- * @since       v1.5.0
  */
 class THM_OrganizerViewAssets extends JView
 {
@@ -38,6 +34,9 @@ class THM_OrganizerViewAssets extends JView
 	 */
 	public function display($tpl = null)
 	{
+		JHTML::_('behavior.modal', 'a.modal-button');
+		JHtml::_('behavior.tooltip');
+
 		$this->items = $this->get('Items');
 		$this->pagination = $this->get('Pagination');
 		$this->state = $this->get('State');
@@ -66,7 +65,6 @@ class THM_OrganizerViewAssets extends JView
 		JToolBarHelper::addNew('coursepool.add', JText::_('COM_THM_ORGANIZER_SUBMENU_ASSETS_ADD_GROUP'));
 		JToolBarHelper::addNew('dummy.add', JText::_('COM_THM_ORGANIZER_SUBMENU_ASSETS_ADD_DUMMY'));
 		JToolBarHelper::editList('course.edit', 'JTOOLBAR_EDIT');
-
 		JToolBarHelper::deleteList('', 'assets.delete', 'JTOOLBAR_DELETE');
 	}
 }
