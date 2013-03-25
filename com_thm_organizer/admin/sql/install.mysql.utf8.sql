86afd69ab41b21fbdfb70c2d997ba2c974355472
James Antrim <james.antrim@mni.thm.de>
2013-03-25 11:05:14 +0100

    degrees query updated manifest standardized and verions incremented

--- com_thm_organizer/admin/sql/install.mysql.utf8.sql
+++ com_thm_organizer/admin/sql/install.mysql.utf8.sql
@@ -162,7 +162,7 @@ CREATE TABLE IF NOT EXISTS `#__thm_organizer_soap_queries` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE`#__thm_organizer_degrees` (
+CREATE TABLE IF NOT EXISTS `#__thm_organizer_degrees` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `abbreviation` varchar(255) NOT NULL,
