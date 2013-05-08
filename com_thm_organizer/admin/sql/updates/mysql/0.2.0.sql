7e9059ed373c76e819471cd5edb2afe7bd823d98
Wolf Rost <Wolf.Rost@mni.thm.de>
2013-05-08 14:00:50 +0200

    -changed drop table order -removed update sql file

--- com_thm_organizer/admin/sql/updates/mysql/0.2.0.sql
+++ com_thm_organizer/admin/sql/updates/mysql/0.2.0.sql
@@ -1,14 +1,13 @@
--- Former curriculum tables with FK constraints have to be removed to alter the
--- signed int key to the more secure unsigned int key standard.
+# Former curriculum tables with FK constraints have to be removed to alter the
+# signed int key to the more secure unsigned int key standard.
 DROP TABLE IF EXISTS `#__thm_organizer_lecturers_assets`;
 DROP TABLE IF EXISTS `#__thm_organizer_assets_semesters`;
 DROP TABLE IF EXISTS `#__thm_organizer_assets_tree`;
 DROP TABLE IF EXISTS `#__thm_organizer_semesters_majors`;
-DROP TABLE IF EXISTS `#__thm_organizer_assets`;
 DROP TABLE IF EXISTS `#__thm_organizer_semesters`;
 DROP TABLE IF EXISTS `#__thm_organizer_curriculum_semesters`;
 
--- Uses MyISAM needs to get dropped for InnoDB
+# Uses MyISAM needs to get dropped for InnoDB
 DROP TABLE IF EXISTS `#__thm_organizer_soap_queries`;
 
 CREATE TABLE IF NOT EXISTS `#__thm_organizer_soap_queries` (
@@ -36,8 +35,8 @@ INSERT INTO `#__thm_organizer_soap_queries` (`id`, `name`, `lsf_object`, `lsf_st
 
 DROP TABLE IF EXISTS `#__thm_organizer_majors`;
 
--- Easier to destroy and rebuild than alter and add. Abbreviation later will
--- provide the link to data from the degrees/majors modeled in Untis departments.
+# Easier to destroy and rebuild than alter and add. Abbreviation later will
+# provide the link to data from the degrees/majors modeled in Untis departments.
 DROP TABLE IF EXISTS `#__thm_organizer_degrees`;
 
 CREATE TABLE`#__thm_organizer_degrees` (
@@ -71,7 +70,7 @@ INSERT INTO `#__thm_organizer_degrees` (`id`, `name`, `abbreviation`) VALUES
 (7, 'Master of Arts', 'M.A.'),
 (8, 'Master of Business Administration and Engineering', 'MBA');
 
--- Alter keys of tables with superficial changes to unsigned int 11
+# Alter keys of tables with superficial changes to unsigned int 11
 ALTER TABLE  `#__thm_organizer_event_exclude_dates`
 CHANGE  `id`  `id` INT( 11 ) UNSIGNED NOT NULL AUTO_INCREMENT ,
 CHANGE  `eventID`  `eventID` INT( 11 ) UNSIGNED NOT NULL;
@@ -89,8 +88,8 @@ CHANGE `id` `id` INT( 11 ) UNSIGNED NOT NULL AUTO_INCREMENT;
 ALTER TABLE  `#__thm_organizer_asset_types`
 CHANGE `id` `id` INT( 11 ) UNSIGNED NOT NULL AUTO_INCREMENT;
 
--- Table has wrong column listed as PK, signed int id, and userid's length is
--- incompatible with the corresponding field in the users table.
+# Table has wrong column listed as PK, signed int id, and userid's length is
+# incompatible with the corresponding field in the users table.
 DROP TABLE IF EXISTS `#__thm_organizer_lecturers`;
 
 CREATE TABLE IF NOT EXISTS `#__thm_organizer_lecturers` (
@@ -104,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `#__thm_organizer_lecturers` (
   UNIQUE KEY (`userid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- Recreate tables with dependencries in reverse order.
+# Recreate tables with dependencries in reverse order.
 CREATE TABLE IF NOT EXISTS `#__thm_organizer_semesters` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -115,6 +114,9 @@ CREATE TABLE IF NOT EXISTS `#__thm_organizer_semesters` (
   PRIMARY KEY (id),
   FOREIGN KEY (`color_id`) REFERENCES `#__thm_organizer_colors` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `#__thm_organizer_assets`;
 
 CREATE TABLE IF NOT EXISTS `#__thm_organizer_assets` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
