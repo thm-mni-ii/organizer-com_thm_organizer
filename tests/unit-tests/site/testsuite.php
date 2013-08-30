fb1148c38f86f329d2385d73dc8144e45790060e
Niklas Simonis <niklas.simonis@mni.thm.de>
2013-08-30 09:37:05 +0200

    *fix Testsuite pipeline

--- tests/unit-tests/site/testsuite.php
+++ tests/unit-tests/site/testsuite.php
@@ -12,7 +12,7 @@
  * @link        www.mni.thm.de
  */
 
-require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'framework_include.php';
+require_once dirname(dirname(dirname(dirname(__FILE__)))).DIRECTORY_SEPARATOR . 'framework_include.php';
 
 /**
  * Testsuite class for site component com_thm_organizer
@@ -25,19 +25,19 @@ require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'framew
  */
 class AllComThmOrganizerSiteTests
 {
-	/**
-	 * Method to initialise the test suite and bind the test files to the suite
-	 * 
-	 * @return  PHPUnit_Framework_TestSuite  The test suite object
-	 */
-	public static function suite()
-	{
-		$suite = new PHPUnit_Framework_TestSuite('Component THM Organizer Site Test');
+    /**
+     * Method to initialise the test suite and bind the test files to the suite
+     *
+     * @return  PHPUnit_Framework_TestSuite  The test suite object
+     */
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Component THM Organizer Site Test');
 
-		// Add Test Files here (example below
-		// $suite->addTestFile(__DIR__ . '/THMOrganizerTestFile.php');
-		$suite->addTestFile(__DIR__ . '/THM_OrganizerModelScheduleSiteTest.php');
-		
-		return $suite;
-	}
+        // Add Test Files here (example below
+        // $suite->addTestFile(__DIR__ . '/THMOrganizerTestFile.php');
+        $suite->addTestFile(__DIR__ . '/THM_OrganizerModelScheduleSiteTest.php');
+
+        return $suite;
+    }
 }
