c0ce2891df1612d6dfecaa47f61fb0893d7bdfb0
Wolf Rost <Wolf.Rost@mni.thm.de>
2014-07-24 17:25:18 +0200

    -Added internet source -Fixed jslint warnings

--- com_thm_organizer/media/js/consumption.js
+++ com_thm_organizer/media/js/consumption.js
@@ -25,6 +25,7 @@ $(document).ready(function ()
 
     function downloadTable(type)
     {
+        var tableToExcel;
         var dt = new Date(),
             day = dt.getDate(),
             month = dt.getMonth() + 1,
@@ -35,39 +36,43 @@ $(document).ready(function ()
             divID = 'thm_organizer-' + type + '-consumption-table',
             sheetName = type + '-' + created;
 
-        var tableToExcel = (function () {
-            var uri = 'data:application/vnd.ms-excel;base64,'
-                , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-                , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
-                , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+        // From http://stackoverflow.com/questions/17126453/html-table-to-excel-javascript (changed slightly)
+        tableToExcel = (function () {
+            var uri = 'data:application/vnd.ms-excel;base64,',
+                template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+                base64 = function (s) {
+                    return window.btoa(unescape(encodeURIComponent(s)))
+                },
+                format = function (s, c) {
+                    return s.replace(/{(\w+)}/g, function (m, p) {
+                        return c[p];
+                    })
+                };
             return function (table, name, filename) {
-                if (!table.nodeType)
-                {
-                    table = document.getElementById(table)
+                if (!table.nodeType) {
+                    table = document.getElementById(table);
                 }
-                var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+                var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML };
 
-                if($.isFunction(window.navigator.msSaveOrOpenBlob))
-                {
+                if ($.isFunction(window.navigator.msSaveOrOpenBlob)) {
                     // IE
                     var fileData = [format(template, ctx)];
                     var blobObject = new Blob(fileData);
                     window.navigator.msSaveOrOpenBlob(blobObject, filename);
                 }
-                else if (navigator.userAgent.indexOf("Safari") >= 0 && navigator.userAgent.indexOf("OPR") === -1 && navigator.userAgent.indexOf("Chrome") === -1)
-                {
+                else if (navigator.userAgent.indexOf("Safari") >= 0 && navigator.userAgent.indexOf("OPR") === -1 && navigator.userAgent.indexOf("Chrome") === -1) {
                     // Safari
                     // No possibility to define the file name :(
                     window.location.href = uri + base64(format(template, ctx));
                 }
-                else
-                {
+                else {
                     // Other Browsers
-                    document.getElementById("dlink").href = uri + base64(format(template, ctx));
-                    document.getElementById("dlink").download = filename;
-                    document.getElementById("dlink").click();
+                    var downloadLink = document.getElementById('dlink');
+                    downloadLink.href = uri + base64(format(template, ctx));
+                    downloadLink.download = filename;
+                    downloadLink.click();
                 }
-            }
+            };
         })();
 
         tableToExcel(divID, type, sheetName + '.xls');
@@ -76,7 +81,7 @@ $(document).ready(function ()
     $('#consumption').keypress(function(e)
     {
         var form = $('#statistic-form');
-        if (e.keyCode == 13)
+        if (e.keyCode === 13)
         {
             form.submit();
         }
@@ -85,6 +90,7 @@ $(document).ready(function ()
 
 function toggleRooms()
 {
+    "use strict";
     var toggleSpan = $("#filter-room-toggle-image");
     if (toggleSpan.hasClass('toggle-closed'))
     {
@@ -99,6 +105,7 @@ function toggleRooms()
 
 function toggleTeachers()
 {
+    "use strict";
     var toggleSpan = $("#filter-teacher-toggle-image");
     if (toggleSpan.hasClass('toggle-closed'))
     {
