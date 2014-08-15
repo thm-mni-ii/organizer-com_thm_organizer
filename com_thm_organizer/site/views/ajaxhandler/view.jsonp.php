6a3da1e6ba57ba7dc9bdea80cf9d7264f915322c
Dominik Bassing <dominik.bassing@mni.thm.de>
2014-08-15 16:20:15 +0200

    Added View again

--- com_thm_organizer/site/views/ajaxhandler/view.jsonp.php
+++ com_thm_organizer/site/views/ajaxhandler/view.jsonp.php
@@ -1,7 +1,94 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: dom
- * Date: 15.08.14
- * Time: 16:11
- */ 
+ * @category    Joomla component
+ * @package     THM_Organizer
+ * @subpackage  com_thm_organizer.site
+ * @name		thm_organizerViewAjaxHandler
+ * @description thm_organizerViewAjaxHandler file from com_thm_organizer
+ * @author      Dominik Bassing, <dominik.bassing@mni.thm.de>
+ * @copyright   2014 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
+ */
+defined('_JEXEC') or die;
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the Giessen Scheduler Component
+ *
+ * @category  Joomla.Component.Site
+ * @package   thm_organizer
+ */
+
+class THM_OrganizerViewAjaxHandler extends JView
+{
+    /**
+     * Method to get extra
+     *
+     * @param   String  $tpl  template
+     *
+     * @return void
+     *
+     * @see JView::display()
+     */
+    public function display($tpl = null)
+    {
+        $model = $this->getModel();
+
+        $task = JRequest::getCmd('scheduletask');
+        $callback = JRequest::getCmd('callback');
+
+        $output = $model->executeTask($task);
+
+        if (count($output) == 1)
+        {
+            $this->response($callback, $output["data"]);
+        }
+        else
+        {
+            $this->response($callback, $output["success"], $output["data"]);
+        }
+    }
+
+    /**
+     * Method to send a response to the client for JSONP
+     *
+     * @param   Object  $mix  The information to send can be a array, string or boolean
+     * @param   Array   $arr  Additional information to send
+     *
+     * @return void
+     */
+    public function response($callback, $mix, $arr = array())
+    {
+        if (is_bool($mix))
+        {
+            if (is_array($arr))
+            {
+                $arr['size'] = count($arr);
+                $arr['success'] = $mix;
+                $arr['sid'] = session_id();
+            }
+        }
+        elseif (is_array($mix))
+        {
+            $arr = $mix;
+            $arr['size'] = count($arr);
+            $arr['sid'] = session_id();
+        }
+        elseif (is_string($mix))
+            $arr = $mix;
+        else
+        {
+            // TODO
+        }
+
+        if (is_array($arr))
+        {
+            echo $callback . '(' . json_encode($arr) . ')';
+        }
+        else
+        {
+            echo $callback . '(' . $arr . ')';
+        }
+    }
+}
