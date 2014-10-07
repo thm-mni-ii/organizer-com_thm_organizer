c1d7d17a86cc1ae2dc7b510150af0ca989e06ba5
James Antrim <james.antrim@mni.thm.de>
2014-10-07 23:23:26 +0200

    readded thm organizer splash page allowed css map files to be added (too much bother not to) added schedules

---
+++ com_thm_organizer/admin/views/thm_organizer/tmpl/default.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @version     v0.1.0
+ * @category    Joomla component
+ * @package     THM_Organizer
+ * @subpackage  com_thm_organizer.admin
+ * @description default template for the thm organizer main menu view
+ * @author      James Antrim, <james.antrim@mni.thm.de>
+ * @author      Daniel Kirsten, <daniel.kirsten@mni.thm.de>
+ * @copyright   2012 TH Mittelhessen
+ * @license     GNU GPL v.2
+ * @link        www.mni.thm.de
+ */
+
+defined('_JEXEC') or die;
+$logoURL = 'media/com_thm_organizer/images/THM_Organizer_Logo_trans.png';
+?>
+<?php
+    echo JHtml::_('image', $logoURL, JText::_('COM_THM_ORGANIZER'), array( 'class' => 'thm_organizer_main_image'));
+?>
+<div id="thm_organizer_main_description" class='thm_organizer_main_description'>
+    <?php echo JText::_("COM_THM_ORGANIZER_MAIN_DESC"); ?>
+</div>
+<div id="cpanel" class='cpanel'>
+<?php foreach ($this->views as $view)
+{
+?>
+    <div class="thm_organizer_main_submenu" >
+        <div class="thm_organizer_main_linkdiv" >
+            <div class="icon">
+                <a href='<?php echo $view['url']; ?>'
+                   class='hasTip' title='<?php echo $view['tooltip']; ?>' >
+<?php
+    echo JHtml::_('image', $view['image'], $view['title'], array( 'class' => 'thm_organizer_main_image'));
+?>
+                    <span><?php echo $view['title']; ?></span>
+                </a>
+            </div>
+        </div>
+    </div>
+<?php
+}
+?>
+</div>
