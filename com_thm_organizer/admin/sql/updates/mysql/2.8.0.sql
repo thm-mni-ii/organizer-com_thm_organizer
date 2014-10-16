64a2223fccbbd4f7ac6480655c34f6ef5084d8cf
James Antrim <james.antrim@mni.thm.de>
2014-10-16 11:31:36 +0200

    small changes, not installable

--- com_thm_organizer/admin/sql/updates/mysql/2.8.0.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__thm_organizer_users` (
-  `id` INT ( 11 ) UNSIGNED NOT NULL,
-  `program_manager` TINYINT ( 1 ) NOT NULL DEFAULT '0',
-  `planner` TINYINT ( 1 ) NOT NULL DEFAULT '0',
-  PRIMARY KEY ( `id` )
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
