6d6cf7857e7191c4d767cf30480234b70d350126
Lavinia Popa-Rössel <lavinia.popa-roessel@mni.thm.de>
2015-06-09 14:28:36 +0200

    Style changes curriculum and subject list. Removed border of language-switcher in stubject-details.
    Change-Id: Ib77f6ccd88fb07fad1586a21fea0b724a6fb8b58

--- com_thm_organizer/media/js/curriculum.js
+++ com_thm_organizer/media/js/curriculum.js
@@ -1,38 +1,42 @@
 /**
  * Created by Lavi on 04.03.2015.
  */
-jQuery(document).ready(function () {
-
+jQuery(document).ready(function ()
+{
     getColorAndChangeCSS();
 
     //Change width of Item if wanted or needed
 /*
-    var itemWidth = "200px";
+    var itemWidth = "170px";
     changeWidthOfItem(itemWidth);
 */
 });
 
 // get background-color of .item-head, get 20% darker color of this and change css properties for boxShadow
-function getColorAndChangeCSS() {
+function getColorAndChangeCSS()
+{
     var x = document.getElementsByClassName("item-head");
     var i;
-    for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++)
+    {
         var bgColor = jQuery(x[i]).css("background-color");
         var darkerColor = shadeRGBColor(bgColor, -0.2);
         x[i].style.boxShadow = ('inset 0px 2px 10px ' + darkerColor);
     }
 }
 
-function shadeRGBColor(color, percent) {
+function shadeRGBColor(color, percent)
+{
     var f = color.split(","), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent, R = parseInt(f[0].slice(4)), G = parseInt(f[1]), B = parseInt(f[2]);
     return "rgb(" + (Math.round((t - R) * p) + R) + "," + (Math.round((t - G) * p) + G) + "," + (Math.round((t - B) * p) + B) + ")";
 }
 
-function changeWidthOfItem(itemWidth) {
+function changeWidthOfItem(itemWidth)
+{
     var x = document.getElementsByClassName("item");
     var i;
-    for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length; i++)
+    {
         x[i].style.width = (itemWidth);
     }
-
 }
