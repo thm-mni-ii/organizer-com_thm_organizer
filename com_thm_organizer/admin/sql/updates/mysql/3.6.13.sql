488e41c746c6ae565068ab2086b3befe22c34f74
James Antrim <james.antrim@ft.thm.de>
2016-08-30 13:30:05 +0200

    default
    Change-Id: I4eb12e5190c33596d4f60e5b0632f29f3d098e02

--- com_thm_organizer/admin/sql/updates/mysql/3.6.13.sql
+++ com_thm_organizer/admin/sql/updates/mysql/3.6.13.sql
@@ -4,43 +4,43 @@ UPDATE `#__thm_organizer_schedules` AS s
     ON s.startDate = pp.startDate AND s.endDate = pp.endDate
 SET s.planningPeriodID = pp.id;
 
-ALTER TABLE `v7ocf_thm_organizer_lesson_configurations`
+ALTER TABLE `#__thm_organizer_lesson_configurations`
   DROP FOREIGN KEY `lesson_configurations_lessonid_fk`;
 
-ALTER TABLE `v7ocf_thm_organizer_lesson_configurations`
+ALTER TABLE `#__thm_organizer_lesson_configurations`
   ADD CONSTRAINT `lesson_configurations_lessonid_fk` FOREIGN KEY (`lessonID`)
-REFERENCES `v7ocf_thm_organizer_lesson_subjects` (`id`)
+REFERENCES `#__thm_organizer_lesson_subjects` (`id`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
-ALTER TABLE `v7ocf_thm_organizer_calendar`
+ALTER TABLE `#__thm_organizer_calendar`
   ADD `delta` VARCHAR(10) NOT NULL DEFAULT '';
 
-ALTER TABLE `v7ocf_thm_organizer_calendar`
+ALTER TABLE `#__thm_organizer_calendar`
   DROP FOREIGN KEY `calendar_configurationid_fk`;
 
-ALTER TABLE `v7ocf_thm_organizer_calendar`
+ALTER TABLE `#__thm_organizer_calendar`
     CHANGE `configurationID` `lessonID` INT (11) UNSIGNED NOT NULL;
 
-ALTER TABLE `v7ocf_thm_organizer_calendar`
+ALTER TABLE `#__thm_organizer_calendar`
   ADD KEY `lessonID` (`lessonID`);
 
-ALTER TABLE `v7ocf_thm_organizer_calendar`
+ALTER TABLE `#__thm_organizer_calendar`
   ADD CONSTRAINT `calendar_lessonid_fk` FOREIGN KEY (`lessonID`)
-REFERENCES `v7ocf_thm_organizer_lessons` (`id`)
+REFERENCES `#__thm_organizer_lessons` (`id`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
-CREATE TABLE IF NOT EXISTS `v7ocf_thm_organizer_calendar_configuration_map` (
+CREATE TABLE IF NOT EXISTS `#__thm_organizer_calendar_configuration_map` (
   `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `calendarID`      INT(11) UNSIGNED NOT NULL,
   `configurationID` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `entry` (`calendarID`, `configurationID`),
-  CONSTRAINT `calendar_configuration_map_calendarID_fk` FOREIGN KEY (`calendarID`) REFERENCES `v7ocf_thm_organizer_calendar` (`id`)
+  CONSTRAINT `calendar_configuration_map_calendarID_fk` FOREIGN KEY (`calendarID`) REFERENCES `#__thm_organizer_calendar` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `calendar_configuration_map_configurationID_fk` FOREIGN KEY (`configurationID`) REFERENCES `v7ocf_thm_organizer_lesson_configurations` (`id`)
+  CONSTRAINT `calendar_configuration_map_configurationID_fk` FOREIGN KEY (`configurationID`) REFERENCES `#__thm_organizer_lesson_configurations` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 )
