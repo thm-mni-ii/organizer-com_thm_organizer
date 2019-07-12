c63ebece35bfe51448c4b0bce80cf994f89eaa05
James Antrim <james.antrim@nm.thm.de>
2019-07-12 09:17:40 +0200

    Re-added text constant required by Joomla. Fixed broken query.
    Change-Id: I9f2cf2a054bfc46209b8232773c9272296f81120

--- com_thm_organizer/admin/sql/updates/mysql/3.14.58.sql
+++ com_thm_organizer/admin/sql/updates/mysql/3.14.58.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `#__thm_organizer_room_types`
     ADD COLUMN `public` TINYINT(1) NOT NULL DEFAULT '1';
 
-UPDATE TABLE `#__thm_organizer_room_types`
+UPDATE `#__thm_organizer_room_types`
 SET public = 0
 WHERE `untisID` = 'BR';
