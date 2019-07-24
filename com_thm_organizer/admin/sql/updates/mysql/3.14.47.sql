a5b98d1f3a8e16978922e0d32780bf00db03f80b
James Antrim <james.antrim@nm.thm.de>
2019-07-24 09:19:29 +0200

    Moved retroactive alteration to the 'current' version.
    Change-Id: I712df40521e618797d7e7364f60e41c0deb9ee7c

--- com_thm_organizer/admin/sql/updates/mysql/3.14.47.sql
+++ com_thm_organizer/admin/sql/updates/mysql/3.14.47.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `#__thm_organizer_subjects`
     ADD COLUMN `bonus_points_de` TEXT NOT NULL,
     ADD COLUMN `bonus_points_en` TEXT NOT NULL;
+
+ALTER TABLE `#__thm_organizer_subjects`
+    MODIFY `creditpoints` DOUBLE(4, 1) UNSIGNED NOT NULL DEFAULT '0';
--- com_thm_organizer/admin/sql/updates/mysql/3.14.48.sql
+++ com_thm_organizer/admin/sql/updates/mysql/3.14.48.sql
@@ -4,6 +4,3 @@ ALTER TABLE `#__thm_organizer_rooms`
 UPDATE `#__menu`
 SET `link` = 'index.php?option=com_thm_organizer&view=schedule_grid'
 WHERE `link` = 'index.php?option=com_thm_organizer&view=schedule';
-
-ALTER TABLE `#__thm_organizer_subjects`
-    MODIFY `creditpoints` DOUBLE(4, 1) UNSIGNED NOT NULL DEFAULT '0';
