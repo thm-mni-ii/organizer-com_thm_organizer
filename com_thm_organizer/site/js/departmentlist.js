c5017b0e2772e5e2dcf910c1f78ad8bf132d8fef
James Antrim <james.antrim@nm.thm.de>
2019-09-20 10:23:08 +0200

    Fixes error where the schedule item settings were not being resolved.
    Change-Id: Iba23fef06b3a5f6f69b11c5990861be5197bf89c

--- com_thm_organizer/site/js/departmentlist.js
+++ com_thm_organizer/site/js/departmentlist.js
@@ -25,28 +25,46 @@ jQuery(document).ready(function () {
      */
     function update(event)
     {
-        var ajaxBaseUrl = '../index.php?option=com_thm_organizer&format=json',
-            ajaxParams = '&departmentIDs=' + department.val() + '&categoryIDs=' + (category.val() || ''),
+        const categoryAjax = new XMLHttpRequest(), groupAjax = new XMLHttpRequest(), roomAjax = new XMLHttpRequest(),
+            url = '../index.php?option=com_thm_organizer&format=json',
+            params = '&departmentIDs=' + department.val() + '&categoryIDs=' + (category.val() || ''),
             keepValue = !event;
 
         // Update programs, when it is not its own trigger
         if (!event || (event && event.target.id !== category.attr('id')))
         {
-            jQuery.ajax(ajaxBaseUrl + '&view=category_options' + ajaxParams)
-                .done(function (request) {
-                    insertOptions(category, request, keepValue);
-                });
+            categoryAjax.open('GET', url + '&view=category_options' + params, true);
+            categoryAjax.onreadystatechange = function () {
+
+                if (categoryAjax.readyState === 4 && categoryAjax.status === 200)
+                {
+                    insertOptions(category, JSON.parse(categoryAjax.responseText), keepValue);
+                }
+            };
+            categoryAjax.send();
         }
+
         // Update pools
-        jQuery.ajax(ajaxBaseUrl + '&view=group_options' + ajaxParams)
-            .done(function (request) {
-                insertOptions(group, request, keepValue);
-            });
+        groupAjax.open('GET', url + '&view=group_options' + params, true);
+        groupAjax.onreadystatechange = function () {
+
+            if (groupAjax.readyState === 4 && groupAjax.status === 200)
+            {
+                insertOptions(group, JSON.parse(groupAjax.responseText), keepValue);
+            }
+        };
+        groupAjax.send();
+
         // Update rooms
-        jQuery.ajax(ajaxBaseUrl + '&view=room_options&roomtypeIDs=-1' + ajaxParams)
-            .done(function (request) {
-                insertOptions(room, request, keepValue);
-            });
+        roomAjax.open('GET', url + '&view=room_options&roomtypeIDs=-1' + params, true);
+        roomAjax.onreadystatechange = function () {
+
+            if (roomAjax.readyState === 4 && roomAjax.status === 200)
+            {
+                insertOptions(room, JSON.parse(roomAjax.responseText), keepValue);
+            }
+        };
+        roomAjax.send();
     }
 
     /**
@@ -55,19 +73,27 @@ jQuery(document).ready(function () {
      * @params {string} request
      * @params {boolean} keepValue
      */
-    function insertOptions(field, request, keepValue)
+    function insertOptions(field, values, keepValue)
     {
-        var values = JSON.parse(request), oldValue = field.val();
+        const oldValues = field.val();
+        let key, option, value;
 
+        // Remove all options other than the placeholder.
         field.find('option:not(:first)').remove();
-        jQuery.each(values, function (name, id) {
-            var option = jQuery('<option></option>').val(id).html(name);
-            if (keepValue && (jQuery.inArray(id, oldValue) !== -1 || id === oldValue))
+
+        for (key in values)
+        {
+            if (values.hasOwnProperty(key))
             {
-                option.attr('selected', 'selected');
+                value = values[key].value;
+                option = jQuery('<option></option>').val(value).html(values[key].text);
+                if (keepValue && (jQuery.inArray(value, oldValues) !== -1 || value === oldValues))
+                {
+                    option.attr('selected', 'selected');
+                }
+                field.append(option);
             }
-            field.append(option);
-        });
+        }
         jQuery(field).chosen('destroy').chosen();
     }
 });
