cb3f78f3cf98cbd4deef5743a296dee4495921c6
James Antrim <james.antrim@nm.thm.de>
2019-10-30 10:42:17 +0100

    Allows the display filters button to be displayed independent of the inclusion of the search field.
    Change-Id: I4bc11d058bd85d4c74af9b01d6aff0c13a30e6dd

--- com_thm_organizer/site/Layouts/HTML/filters-search.php
+++ com_thm_organizer/site/Layouts/HTML/filters-search.php
@@ -16,8 +16,9 @@ use Organizer\Helpers\Languages;
 // Receive overridable options
 $options = empty($options) ? [] : $options;
 
-if (is_array($options)) {
-    $options = new Registry($options);
+if (is_array($options))
+{
+	$options = new Registry($options);
 }
 
 // Options
@@ -28,35 +29,37 @@ $filters = $this->filterForm->getGroup('filter');
 ?>
 
 <?php if (!empty($filters['filter_search'])) : ?>
-    <?php if ($searchButton) : ?>
+	<?php if ($searchButton) : ?>
         <label for="filter_search" class="element-invisible">
-            <?php echo Languages::_('THM_ORGANIZER_SEARCH'); ?>
+			<?php echo Languages::_('THM_ORGANIZER_SEARCH'); ?>
         </label>
         <div class="btn-wrapper input-append">
-            <?php echo $filters['filter_search']->input; ?>
-            <?php if ($filters['filter_search']->description) : ?>
-                <?php JHtmlBootstrap::tooltip('#filter_search',
-                    array('title' => Languages::_($filters['filter_search']->description))); ?>
-            <?php endif; ?>
+			<?php echo $filters['filter_search']->input; ?>
+			<?php if ($filters['filter_search']->description) : ?>
+				<?php JHtmlBootstrap::tooltip('#filter_search',
+					array('title' => Languages::_($filters['filter_search']->description))); ?>
+			<?php endif; ?>
             <button type="submit" class="btn hasTooltip"
                     title="<?php echo Languages::tooltip('THM_ORGANIZER_SEARCH'); ?>"
                     aria-label="<?php echo Languages::_('THM_ORGANIZER_SEARCH'); ?>">
                 <span class="icon-search" aria-hidden="true"></span>
             </button>
         </div>
-        <?php if ($filterButton) : ?>
-            <div class="btn-wrapper hidden-phone">
-                <button type="button" class="btn hasTooltip js-stools-btn-filter"
-                        title="<?php echo Languages::tooltip('THM_ORGANIZER_SEARCH_TOOLS_DESC'); ?>">
-                    <?php echo Languages::_('THM_ORGANIZER_SEARCH_TOOLS'); ?> <span class="caret"></span>
-                </button>
-            </div>
-        <?php endif; ?>
-        <div class="btn-wrapper">
-            <button type="button" class="btn hasTooltip js-stools-btn-clear"
-                    title="<?php echo Languages::tooltip('THM_ORGANIZER_RESET'); ?>">
-                <?php echo Languages::_('THM_ORGANIZER_RESET'); ?>
-            </button>
-        </div>
-    <?php endif; ?>
+	<?php endif; ?>
+<?php endif; ?>
+<?php if ($filterButton) : ?>
+    <div class="btn-wrapper hidden-phone">
+        <button type="button" class="btn hasTooltip js-stools-btn-filter"
+                title="<?php echo Languages::tooltip('THM_ORGANIZER_SEARCH_TOOLS_DESC'); ?>">
+			<?php echo Languages::_('THM_ORGANIZER_SEARCH_TOOLS'); ?> <span class="caret"></span>
+        </button>
+    </div>
+<?php endif; ?>
+<?php if (!empty($filters['filter_search'])) : ?>
+    <div class="btn-wrapper">
+        <button type="button" class="btn hasTooltip js-stools-btn-clear"
+                title="<?php echo Languages::tooltip('THM_ORGANIZER_RESET'); ?>">
+			<?php echo Languages::_('THM_ORGANIZER_RESET'); ?>
+        </button>
+    </div>
 <?php endif;
