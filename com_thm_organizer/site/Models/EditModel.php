b18644e38dfbbd3d9d2fb8eba95ed5d5d4acbd13
James Antrim <james.antrim@nm.thm.de>
2019-11-06 10:51:18 +0100

    Adds missing import.
    Change-Id: I19b1b41d4335e80185b6b2c2b9cedcdd167e86a8

--- com_thm_organizer/site/Models/EditModel.php
+++ com_thm_organizer/site/Models/EditModel.php
@@ -16,6 +16,7 @@ use Joomla\CMS\MVC\Model\AdminModel;
 use Joomla\CMS\Table\Table;
 use Organizer\Helpers\Access;
 use Organizer\Helpers\Input;
+use Organizer\Helpers\Languages;
 use Organizer\Helpers\Named;
 use Organizer\Helpers\OrganizerHelper;
 
@@ -24,133 +25,136 @@ use Organizer\Helpers\OrganizerHelper;
  */
 abstract class EditModel extends AdminModel
 {
-    use Named;
+	use Named;
 
-    protected $deptResource;
+	protected $deptResource;
 
-    public $item = null;
+	public $item = null;
 
-    public $tableName = null;
+	public $tableName = null;
 
-    /**
-     * Constructor.
-     *
-     * @param array $config An optional associative array of configuration settings.
-     */
-    public function __construct($config = [])
-    {
-        parent::__construct($config);
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 */
+	public function __construct($config = [])
+	{
+		parent::__construct($config);
 
-        $this->setContext();
-    }
+		$this->setContext();
+	}
 
-    /**
-     * Provides a strict access check which can be overwritten by extending classes.
-     *
-     * @return bool  true if the user can access the view, otherwise false
-     */
-    protected function allowEdit()
-    {
-        return Access::isAdmin();
-    }
+	/**
+	 * Provides a strict access check which can be overwritten by extending classes.
+	 *
+	 * @return bool  true if the user can access the view, otherwise false
+	 */
+	protected function allowEdit()
+	{
+		return Access::isAdmin();
+	}
 
-    /**
-     * Method to get the form
-     *
-     * @param array $data     Data         (default: array)
-     * @param bool  $loadData Load data  (default: true)
-     *
-     * @return mixed Form object on success, False on error.
-     * @throws Exception => unauthorized access
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function getForm($data = [], $loadData = true)
-    {
-        $allowEdit = $this->allowEdit();
-        if (!$allowEdit) {
-            throw new Exception(Languages::_('THM_ORGANIZER_401'), 401);
-        }
+	/**
+	 * Method to get the form
+	 *
+	 * @param   array  $data      Data         (default: array)
+	 * @param   bool   $loadData  Load data  (default: true)
+	 *
+	 * @return mixed Form object on success, False on error.
+	 * @throws Exception => unauthorized access
+	 *
+	 * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+	 */
+	public function getForm($data = [], $loadData = true)
+	{
+		$allowEdit = $this->allowEdit();
+		if (!$allowEdit)
+		{
+			throw new Exception(Languages::_('THM_ORGANIZER_401'), 401);
+		}
 
-        $name = $this->get('name');
-        $form = $this->loadForm($this->context, $name, ['control' => 'jform', 'load_data' => $loadData]);
+		$name = $this->get('name');
+		$form = $this->loadForm($this->context, $name, ['control' => 'jform', 'load_data' => $loadData]);
 
-        return empty($form) ? false : $form;
-    }
+		return empty($form) ? false : $form;
+	}
 
-    /**
-     * Method to get a single record.
-     *
-     * @param integer $pk The id of the primary key.
-     *
-     * @return mixed    Object on success, false on failure.
-     * @throws Exception => unauthorized access
-     */
-    public function getItem($pk = null)
-    {
-        // Prevents duplicate execution from getForm and getItem
-        if (isset($this->item->id) and ($this->item->id === $pk or $pk === null)) {
-            return $this->item;
-        }
+	/**
+	 * Method to get a single record.
+	 *
+	 * @param   integer  $pk  The id of the primary key.
+	 *
+	 * @return mixed    Object on success, false on failure.
+	 * @throws Exception => unauthorized access
+	 */
+	public function getItem($pk = null)
+	{
+		// Prevents duplicate execution from getForm and getItem
+		if (isset($this->item->id) and ($this->item->id === $pk or $pk === null))
+		{
+			return $this->item;
+		}
 
-        $this->item = parent::getItem($pk);
-        $allowEdit  = $this->allowEdit();
+		$this->item = parent::getItem($pk);
+		$allowEdit  = $this->allowEdit();
 
-        if (!$allowEdit) {
-            throw new Exception(Languages::_('THM_ORGANIZER_401'), 401);
-        }
+		if (!$allowEdit)
+		{
+			throw new Exception(Languages::_('THM_ORGANIZER_401'), 401);
+		}
 
-        return $this->item;
-    }
+		return $this->item;
+	}
 
-    /**
-     * Method to get a table object, load it if necessary.
-     *
-     * @param string $name    The table name. Optional.
-     * @param string $prefix  The class prefix. Optional.
-     * @param array  $options Configuration array for model. Optional.
-     *
-     * @return Table  A Table object
-     */
-    public function getTable($name = '', $prefix = '', $options = array())
-    {
-        $name         = OrganizerHelper::getClass($this);
-        $resourceName = str_replace('Edit', '', $name);
-        $tableName    = OrganizerHelper::getPlural($resourceName);
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $name     The table name. Optional.
+	 * @param   string  $prefix   The class prefix. Optional.
+	 * @param   array   $options  Configuration array for model. Optional.
+	 *
+	 * @return Table  A Table object
+	 */
+	public function getTable($name = '', $prefix = '', $options = array())
+	{
+		$name         = OrganizerHelper::getClass($this);
+		$resourceName = str_replace('Edit', '', $name);
+		$tableName    = OrganizerHelper::getPlural($resourceName);
 
-        return OrganizerHelper::getTable($tableName);
-    }
+		return OrganizerHelper::getTable($tableName);
+	}
 
-    /**
-     * Method to get a form object.
-     *
-     * @param string  $name    The name of the form.
-     * @param string  $source  The form source. Can be XML string if file flag is set to false.
-     * @param array   $options Optional array of options for the form creation.
-     * @param boolean $clear   Optional argument to force load a new form.
-     * @param string  $xpath   An optional xpath to search for the fields.
-     *
-     * @return  Form|boolean  \JForm object on success, false on error.
-     */
-    protected function loadForm($name, $source = null, $options = array(), $clear = false, $xpath = false)
-    {
-        Form::addFormPath(JPATH_COMPONENT_SITE . '/Forms');
-        Form::addFieldPath(JPATH_COMPONENT_SITE . '/Fields');
+	/**
+	 * Method to get a form object.
+	 *
+	 * @param   string   $name     The name of the form.
+	 * @param   string   $source   The form source. Can be XML string if file flag is set to false.
+	 * @param   array    $options  Optional array of options for the form creation.
+	 * @param   boolean  $clear    Optional argument to force load a new form.
+	 * @param   string   $xpath    An optional xpath to search for the fields.
+	 *
+	 * @return  Form|boolean  \JForm object on success, false on error.
+	 */
+	protected function loadForm($name, $source = null, $options = array(), $clear = false, $xpath = '')
+	{
+		Form::addFormPath(JPATH_COMPONENT_SITE . '/Forms');
+		Form::addFieldPath(JPATH_COMPONENT_SITE . '/Fields');
 
-        return parent::loadForm($name, $source, $options, $clear, $xpath);
-    }
+		return parent::loadForm($name, $source, $options, $clear, $xpath);
+	}
 
-    /**
-     * Method to load the form data.
-     *
-     * @return object
-     * @throws Exception => unauthorized access
-     */
-    protected function loadFormData()
-    {
-        $resourceIDs = Input::getSelectedIDs();
-        $resourceID  = empty($resourceIDs) ? 0 : $resourceIDs[0];
+	/**
+	 * Method to load the form data.
+	 *
+	 * @return object
+	 * @throws Exception => unauthorized access
+	 */
+	protected function loadFormData()
+	{
+		$resourceIDs = Input::getSelectedIDs();
+		$resourceID  = empty($resourceIDs) ? 0 : $resourceIDs[0];
 
-        return $this->getItem($resourceID);
-    }
+		return $this->getItem($resourceID);
+	}
 }
